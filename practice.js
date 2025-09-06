43e685da92bb4dddc4373d750738f7c2e42212da
kusumakonchada <kusumakonchada123@gmail.com>
2025-09-06 12:37:04 +0530

    datatype

--- practice.js
+++ practice.js
@@ -5,3 +5,31 @@ console.log(name)
 
 let num=18;
 console.log(num)
+
+let x;
+console.log(x)
+
+let y=null;
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+
+let islogged=false
+console.log(islogged)
+
+console.log(typeof islogged)
+console.log(typeof x)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person={
+    name:"kusuma",
+
+    age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
