10ad80fd40c9821b47b8ea9f09c74298b9f61c38
kusumakonchada <kusumakonchada123@gmail.com>
2025-09-06 15:54:20 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,27 @@
+function greet(){
+    console.log("welcome to Dev Astra")
+}
+greet()
+
+function add (x,y){
+      return x+y;
+}
+let result=add(18,19)
+console.log(result);
+
+const pi=3.14;
+console.log(pi);
+
+const multiply=function(x,y){
+    return x*y;
+}
+console.log(multiply(5,3));
+
+const square=(x)=>x*x;
+console.log(square(5));
+const subtract =(x,y)=>{
+    console.log(x,y);
+    return x-y;
+
+}
+console.log(subtract(20,15));
